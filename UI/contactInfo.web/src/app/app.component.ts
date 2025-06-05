cbc797016018ebe2233b85c3a6fe6ffd0c649b35
cameronojala24 <camojala@gmail.com>
2025-06-05 16:29:24 -0400

    Added form input and functionality

--- UI/contactInfo.web/src/app/app.component.ts
+++ UI/contactInfo.web/src/app/app.component.ts
@@ -4,18 +4,44 @@ import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Contact } from '../models/contact.model';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   http = inject(HttpClient);
 
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone: new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false)
+  });
+
   contacts$ = this.getContacts();
+
+  onFormSubmit() {
+    const addContactRequest = {
+      name: this.contactsForm.value.name,
+      email: this.contactsForm.value.email,
+      phone: this.contactsForm.value.phone,
+      favorite: this.contactsForm.value.favorite,
+    };
+
+    this.http.post('https://localhost:7117/api/Contacts', addContactRequest)
+    .subscribe({
+      next: (value) => {
+        console.log(value)
+        this.contacts$ = this.getContacts();
+        this.contactsForm.reset();
+      }
+    });
+  }
 
   private getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>('https://localhost:7117/api/Contacts');
